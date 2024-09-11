0cfc782d8e11b51a8515ac1d7793f08b95b4ed53
Shubh-2909 <shubhshubhanjal96@gmail.com>
2024-09-11 15:41:10 +0530

    Sidebar

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -11,17 +11,13 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1">
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
           <Image src="/spooky.svg" alt="VibeCast" height="32" width="32" />
         </div>
-        <div className={cn(font.className)}>
-            <p className="text-lg font-semibold">
-                VibeCast
-            </p>
-            <p className="text-xs text-muted-foreground">
-                Let's Stream
-            </p>
+        <div className={cn("hidden lg:block", font.className)}>
+          <p className="text-lg font-semibold">VibeCast</p>
+          <p className="text-xs text-muted-foreground">Let's Stream</p>
         </div>
       </div>
     </Link>
