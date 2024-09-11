cc7e0b10b18110c00d72b9f8b92bafb18d7483b7
Shubh-2909 <shubhshubhanjal96@gmail.com>
2024-09-12 02:31:01 +0530

    Implemented follow and unfollow feature

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -4,6 +4,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import React from "react";
 import { Hint } from "@/components/hint";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
@@ -38,3 +39,12 @@ const Toggle = () => {
 };
 
 export default Toggle;
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+      <Skeleton className="h-6 w-[100px]" />
+      <Skeleton className="h-6 w-6" />
+    </div>
+  );
+};
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -3,11 +3,48 @@ import { getSelf } from "./auth-service";
 import { resolve } from "path";
 
 export const getRecommended = async () => {
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  let userId;
+
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        AND: [
+          {
+            NOT: {
+              id: userId,
+            },
+          },
+          {
+            NOT: {
+              followedBy: {
+                some: {
+                  followerId: userId,
+                },
+              },
+            },
+          },
+        ],
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
